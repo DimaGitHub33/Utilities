25258ee55c9272aa2c20f90740da3390aa0f8084
Dima <dima.github33@gmail.com>
2022-08-31 10:37:48 +0300

    Update

--- Run.py
+++ Run.py
@@ -1,36 +1,53 @@
-#%% Import Data
+#%% Import Data ------------------------------------------------------------
 import pandas as pd
 import numpy as np
 data = pd.read_csv('http://winterolympicsmedals.com/medals.csv')
 data.head(10)
 
-# %% Working Directory
+# %% Working Directory -----------------------------------------------------
 import os
 os.chdir("/Users/dhhazanov/Documents/GitHub/Utilities/")
 os.getcwd()
 
-# %% Import for Utilities Functions
+# %% Import for Utilities Functions ----------------------------------------
 from NumericYMC import RJitter,Ranks_Dictionary,FourBasicNumericYMC,NumericYMC,FactorYMC,percentile
+
+# %% Rjitter ---------------------------------------------------------------
 print("RJitter:")
 print(RJitter(data['Year'],factor=0.1))
-print("\n")
-print("\n")
+
+# %% Ranks Dictionary -------------------------------------------------------
 print("Ranks_Dictionary:")
 print(Ranks_Dictionary(data['Year'],ranks_num=10))
-print("\n")
-print("\n")
-print("NumericYMC:")
+
+# %% NumericYMC -------------------------------------------------------------
+## Four Basic Numeric YMC in one table
+print("FourBasicNumericYMC:")
 print(FourBasicNumericYMC(Variable = data['Year'],Target = np.where(data['Year']>=data['Year'].mean(),1,0),NumberOfGroups = 10))
 print("\n")
-print("\n")
+
+## Mean
+print("Mean YMC:")
 print(NumericYMC(Variable = data['Year'],Target = np.where(data['Year']>=data['Year'].mean(),1,0),NumberOfGroups = 10,Fun = np.mean,Name = "Mean"))
+print("\n")
+
+## Median
+print("Median YMC:")
 print(NumericYMC(Variable = data['Year'],Target = np.where(data['Year']>=data['Year'].mean(),1,0),NumberOfGroups = 10,Fun = np.median,Name = "Median"))
-#print(NumericYMC(Variable = data['Year'],Target = np.where(data['Year']>=data['Year'].mean(),1,0),NumberOfGroups = 10,Fun = percentile(90),Name = "Percentile"))
 print("\n")
+
+## Percentile
+print("Percentile YMC:")
+print(NumericYMC(Variable = data['Year'],Target = np.where(data['Year']>=data['Year'].mean(),1,0),NumberOfGroups = 10,Fun = percentile(90),Name = "Percentile"))
 print("\n")
-print("FactorYMC:")
+
+# %% FactorYMC -------------------------------------------------------------
+## FactorYMC - Sport
+print("FactorYMC - Sport:")
 print(FactorYMC(VariableToConvert = 'Sport', TargetName = 'Year',Data = data, FrequencyNumber = 100, Fun = np.median, Suffix='_Median_YMC' ))
-print("\n")
-print("\n")
+
+## FactorYMC - Event gender 
+print("FactorYMC - Event gender :")
+print(FactorYMC(VariableToConvert = 'Event gender', TargetName = 'Year',Data = data, FrequencyNumber = 100, Fun = np.median, Suffix='_Median_YMC' ))
 
 # %%
